4186ecb28832329aec20e30c79a8e11c005d2a1f
AnnaTupota <annatupotaalexxandrovna@mail.ru>
2024-10-16 21:54:15 +0600

    added the card component and the logic of transferring the number of cards

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import React, { useState } from 'react';
 import './App.css';
-
 import Header from './components/ui/Header/Header';
+import Card from './components/ui/Card/Card';
 
 function App() {
+  const [cardsNumber, setCardsNumber] = useState(0);
+
+  const handleGetFacts = (number) => {
+    setCardsNumber(number);
+  };
+
   return (
-    <div className="App">
-      <Header />
+    <div className="site-wrapper">
+      <Header onGetFacts={handleGetFacts} />
+      <div className="cards-wrapper">
+        <div className="cards-container">
+          {Array.from({ length: cardsNumber }).map((_, index) => (
+            <Card key={index} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
