e217309899c165deceb50cbf439f16c162a70b01
AnnaTupota <annatupotaalexxandrovna@mail.ru>
2025-01-25 16:48:58 +0600

    changing the location of the cards

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import React, { useState } from 'react';
+
 import './App.css';
+
 import Header from './components/ui/Header/Header';
 import Card from './components/ui/Card/Card';
 
 function App() {
   const [cardsNumber, setCardsNumber] = useState(0);
 
-  const handleGetFacts = (number) => {
+  const updateFactsNumber = (number) => {
     setCardsNumber(number);
   };
 
   return (
-    <div className="site-wrapper">
-      <Header onGetFacts={handleGetFacts} />
-      <div className="cards-wrapper">
-        <div className="cards-container">
-          {Array.from({ length: cardsNumber }).map((_, index) => (
-            <Card key={index} />
-          ))}
+    <div className="wrapper">
+      <div className="site-wrapper">
+        <Header onGetFacts={updateFactsNumber} />
+        <div className="cards-wrapper">
+          <div className="cards-container">
+            {Array.from({ length: cardsNumber }).map((_, index) => (
+              <Card key={index} />
+            ))}
+          </div>
         </div>
       </div>
     </div>
